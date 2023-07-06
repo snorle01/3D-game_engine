32b79d03f476cb69d350f962a5e7e0aad6faf329
snorle01 <109174935+snorle01@users.noreply.github.com>
2023-07-06 13:18:17 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+import pygame as pg
+import moderngl as mgl
+import sys
+from model import *
+from camera import Camera
+from light import Light
+from mesh import Mesh
+from scene import Scene
+from scene_renderer import SceneRenderer
+
+class GraphichEngine:
+    def __init__(self, win_size=(1000, 600)):
+        # init pygame modules
+        pg.init()
+        # window size
+        self.WIN_SIZE = win_size
+        # set opengl attr
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+        # create opengl context
+        pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
+        # mouse settings
+        pg.event.set_grab(True)
+        pg.mouse.set_visible(False)
+        # detect and use exsisting opengl context
+        self.ctx = mgl.create_context()
+        # self.ctx.front_face = 'cw'
+        self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
+        # create an object to help track time
+        self.clock = pg.time.Clock()
+        self.time = 0
+        self.delta_time = 0
+        # light
+        self.light = Light()
+        # camera
+        self.camera = Camera(self)
+        # mesh
+        self.mesh = Mesh(self)
+        # scene
+        self.scene = Scene(self)
+        # renderer
+        self.scene_renderer = SceneRenderer(self)
+
+    def check_event(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                self.scene.destroy()
+                pg.quit()
+                sys.exit()
+
+    def render(self):
+        # clear framebuffer
+        self.ctx.clear(color=(0.08, 0.16, 0.18))
+        # render scene
+        self.scene_renderer.render()
+        # swap buffers
+        pg.display.flip()
+
+    def get_time(self):
+        self.time = pg.time.get_ticks() * 0.001
+
+    def run(self):
+        while True:
+            self.get_time()
+            self.check_event()
+            self.camera.update()
+            self.render()
+            self.delta_time = self.clock.tick(0)
+            pg.display.set_caption(f'{self.clock.get_fps() :.0f}')
+
+if __name__ == '__main__':
+    app = GraphichEngine()
+    app.run()
